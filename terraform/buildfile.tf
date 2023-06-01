e52cb8084944e8fd1122043a9461adc073a375ad
NoahKirchner <noahthomkirchner@gmail.com>
2023-06-01 09:45:29 -0400

    Temporary fix for vm name in order to test the rest of the functionality

--- terraform/buildfile.tf
+++ terraform/buildfile.tf
@@ -2,7 +2,7 @@ variable "range_configuration" {
     default = {
         "win10-workstation" = {
             vm_count = 2
-            name = "tf-win10-pro-${count.index}"
+            name = "tf-win10-pro"
             target_node = "r730"
             clone = "Windows10Pro"
             os_type = "win10"
