a538a5a04a1498d15d1656a135ac3bd63ac1528a
NoahKirchner <noahthomkirchner@gmail.com>
2023-06-01 09:59:38 -0400

    Uses a for loop inside of foreach

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ build_list = distinct(flatten([
 }
 
 resource "proxmox_vm_qemu" "virtual_machines" {
-    for_each = local.build_list
+    for_each = { for entry in local.build_list}
 
     name = each.value.name
     target_node = each.value.target_node
