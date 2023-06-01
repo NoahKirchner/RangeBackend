6e2e4b5c0ce92b2749a44f89ec4eba180a0c02ee
NoahKirchner <noahthomkirchner@gmail.com>
2023-06-01 11:02:16 -0400

    Removes toset

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ build_list = distinct(flatten([
 }
 
 resource "proxmox_vm_qemu" "virtual_machines" {
-    for_each = toset(local.build_list)
+    for_each = local.build_list
 
     name = each.value.name
     target_node = each.value.target_node
