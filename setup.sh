d6d3bbcd03c49140d09da166a52e3d3463fb5b69
NoahKirchner <noahthomkirchner@gmail.com>
2023-06-06 16:04:43 -0400

    Fix an oopsie poopsie

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-./scripts/download-images.sh
-./terraform/terraform-initial-setup.sh
-./terraform/deploy-blank-images.sh
+(cd ./scripts && ./download-images.sh)
+(cd ./terraform && ./terraform-initial-setup.sh)
+(cd ./terraform && ./deploy-blank-images.sh)
 
 echo "A group of images have just been deployed to terraform with the prefix 'blank'. Go through the installation process on them and ensure that you use the same username and password for all of them."
