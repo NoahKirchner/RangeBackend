aae4aecc13d915eba67043ad71cb26832bba22ba
NoahKirchner <noahthomkirchner@gmail.com>
2023-06-06 21:46:44 -0400

    Adds environment file to be added to the packer directory

--- terraform/terraform-initial-setup.sh
+++ terraform/terraform-initial-setup.sh
@@ -12,7 +12,7 @@ if ! command -v pveum &>/dev/null; then
 fi
 
 pveum user delete terraform-prov@pve
-pveum role delete TerraformProv 
+pveum role delete TerraformProv
 
 pveum role add TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt"
 pveum user add terraform-prov@pve --password $password 
@@ -25,6 +25,9 @@ echo 'PM_USER="terraform-prov@pve"' > credentials.tfvars
 echo -n 'PM_PASSWORD="' >> credentials.tfvars
 echo -n $password >> credentials.tfvars
 echo  '"' >> credentials.tfvars
+
+echo "PROXMOX_USERNAME=terraform-prov@pve" > ../packer/credentials.env
+echo "PROXMOX_PASSWORD=$password" >> ../packer/credentials.env
 
 (cd ./core-topology && terraform init)
 
